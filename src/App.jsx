3a41c5c5258ac2994697ba4715390753fd1c1356
salazarkendall <kendallsalazarvargas@gmail.com>
2024-07-31 07:14:47 -0600

    jsx intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 const App = () => {
-  return <div className="text-5xl">Hello</div>;
+  const name = "Kendall Salazar";
+  const names = ["Alina", "Jef", "Gabo", "David"];
+  const loggedIn = false;
+  const helloStyle = {
+    color: "red",
+    fontSize: "6px",
+  };
+
+  return (
+    <>
+      <div className="text-5xl">Hello</div>
+      <p style={helloStyle}>{name}</p>
+      <ul>
+        {names.map((name, idx) => {
+          return <li key={idx}>{name}</li>;
+        })}
+      </ul>
+      {loggedIn ? <h1>Is Logged</h1> : <h1>Not Logged</h1>}
+    </>
+  );
 };
 
 export default App;
