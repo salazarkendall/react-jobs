53c59de18a3d65f414dfe87c29feb881b2bd28f8
salazarkendall <kendallsalazarvargas@gmail.com>
2024-07-31 12:41:35 -0600

    added: useEffect in JobPage

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,7 +9,7 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = "http://localhost:8000/jobs";
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
